a65dbf7a295490c2125b63859fe9ddac8d5f9633
Binh Nguyen <binhnguyen7968@gmail.com>
2016-10-26 10:11:18 +0700

    add oauth

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  },
+  oauth: {
+   facebook: {
+     appIds: "FACEBOOK APP ID"
+   },
+   google: true
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
